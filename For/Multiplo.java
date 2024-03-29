6eb18312d26cd44f91d636ed4a8614bb588e62c0
Barbara  Samira Santiesteban Saravia <145080442+BarbaraSamiraS@users.noreply.github.com>
2024-03-29 12:20:44 -0300

    Create Multiplo.java

---
+++ For/Multiplo.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Multiplo {
+	public static void main(String[] args) {
+		Scanner ler = new Scanner (System.in);
+		int i;
+		
+		for (i=1;i<=100;i++) {
+			
+			
+			if (i % 10 == 0) {
+				System.out.print(+i);
+				System.out.println("     Multiplo de 10");
+			} else {
+				System.out.println(+i);
+			}
+		}
+		ler.close();
+	}
+
+}
